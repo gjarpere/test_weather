147d3dbddccb33ae6728b20d3a02e238bc02e94c
Aleksandr Martynov <alexm@eze.tech>
2017-05-22 17:03:20 +0300

    Project created

---
+++ Test_weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test_weather
+//
+//  Created by Aleksandr Martynov on 5/22/17.
+//  Copyright © 2017 Aleksandr Martynov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test_weather/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Test_weather
+//
+//  Created by Aleksandr Martynov on 5/22/17.
+//  Copyright © 2017 Aleksandr Martynov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
